72e48da80fe9faa64ec22d26e117f45b9b236d45
sangrit-siit <Kittikom.San@dome.tu.ac.th>
2020-03-21 15:54:02 +0700

    init project

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
-from app import app, db
+from app import app, db, migrate
 from models import Device, SensorData
-
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, Device=Device, SensorData=SensorData)
+    return dict(app=app, db=db, Device=Device, SensorData=SensorData, migrate=migrate)
