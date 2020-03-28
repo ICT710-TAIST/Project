a952721130d3742389418b77009770ab36e73e19
sangrit-siit <Kittikom.San@dome.tu.ac.th>
2020-03-28 14:09:00 +0700

    change data model

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import app, db, migrate
-from models import Device, SensorData
+from models import SensorData
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, Device=Device, SensorData=SensorData, migrate=migrate)
+    return dict(app=app, db=db, SensorData=SensorData, migrate=migrate)
--- models.py
+++ models.py
@@ -1,72 +1,56 @@
 from app import db
 import datetime
 
-class Device(db.Model):
-
-    __tablename__ = 'device'
-
-    device_name = db.Column(db.String(255), primary_key=True)
-    device_client_id = db.Column(db.String(255))
-    sensor_data = db.relationship(
-        'SensorData',
-        backref='device',
-        lazy='dynamic'
-    )
-
-    def __init__(self, device_name, device_client_id):
-        self.device_name = device_name
-        self.device_client_id = device_client_id
-
-    def __repr__(self):
-        return '<device_name \'{}\'>'.format(self.device_name)
-
-    def serialize(self):
-        return {
-            'device_id': self.device_id,
-            'device_name': self.device_name,
-            'device_client_id': self.device_client_id
-        }
-
 class SensorData(db.Model):
 
     __table_name__ = 'sensor_data'
 
-    sensor_data_id = db.Column(db.Integer, primary_key=True)
-    device_name = db.Column(db.String(255), db.ForeignKey('device.device_name'))
+    id          = db.Column(db.Integer(), primary_key=True)
+    device_id   = db.Column(db.Integer())
     roll        = db.Column(db.Float())
     pitch       = db.Column(db.Float())
     yaw         = db.Column(db.Float())
     acc_x       = db.Column(db.Float())
     acc_y       = db.Column(db.Float())
     acc_z       = db.Column(db.Float())
+    label       = db.Column(db.Integer())
+    type        = db.Column(db.String())
     timestamp   = db.Column(db.DateTime(), nullable=False, default=datetime.datetime.now)
 
-    def __init__(self, roll, pitch, yaw, acc_x, acc_y, acc_z):
+    def __init__(self, device_id, roll, pitch, yaw, acc_x, acc_y, acc_z, label, type):
+        self.device_id = device_id
         self.roll  = roll
         self.pitch = pitch
         self.yaw   = yaw
         self.acc_x = acc_x
         self.acc_y = acc_y
         self.acc_z = acc_z
+        self.label = label
+        self.type  = type
 
     def __repr__(self):
-        return '<roll {}, pitch {}, yaw {}, acc_x {}, acc_y {}, acc_z {}, timestamp {}>'.format(
+        return '<device_id {}, roll {}, pitch {}, yaw {}, acc_x {}, acc_y {}, acc_z {}, label {}, type {}, timestamp {}>'.format(
+            self.device_id,
             self.roll,
             self.pitch,
             self.yaw,
             self.acc_x,
             self.acc_y,
             self.acc_z,
+            self.label,
+            self.type,
             self.timestamp)
 
     def serialize(self):
         return {
-            'device_name': self.device_name,
+            'device_id': self.device_id,
             'roll': self.roll,
             'pitch': self.pitch,
             'yaw': self.yaw,
             'acc_x': self.acc_x,
             'acc_y': self.acc_y,
             'acc_z': self.acc_z,
+            'label': self.label,
+            'type' : self.type,
             'timestamp': self.timestamp
         }
